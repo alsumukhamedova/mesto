3058fb016274e5aeb51ba12a529245277d205522
Alsu M <alsumukhamedova@yandex.ru>
2021-07-19 20:05:40 +0300

    отправить на первую проверку

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
-let profile = document.querySelector('.profile');
-let editButton = profile.querySelector('.profile__edit-button');
-const profilePart = profile.querySelector('.profile__part');
-const profileInfo = profile.querySelector('.profile__info');
-const popupClose = document.querySelector('.popup__close');
-const formElement = document.querySelector('.popup__form');
+let popup = document.querySelector('.popup');
+let popupOpened = document.querySelector('.profile__edit-button');
+let popupClose = document.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_type_name');
+let descriptionInput = document.querySelector('.popup__input_type_description');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+
+
 
 function openPopup() {
-    let nameInput = document.querySelector('.popup__input_type_name');
-    let descriptionInput = document.querySelector('.popup__input_type_description');
-    let popupOpened = document.querySelector('.popup');
-
-    nameInput.value = profile.querySelector('.profile__name').textContent;
-    descriptionInput.value = profile.querySelector('.profile__description').textContent;
-    popupOpened.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    descriptionInput.value = profileDescription.textContent;
+    popup.classList.add('popup_opened');
 }
 function closePopup() {
-    let nameInput = document.querySelector('.popup__input_type_name');
-    let descriptionInput = document.querySelector('.popup__input_type_description');
-    let popupOpened = document.querySelector('.popup');
-    popupOpened.classList.remove('popup_opened');
+    popup.classList.remove('popup_opened');
     nameInput.value = '';
     descriptionInput.value = '';
 }
 function addInfo(evt) {
     evt.preventDefault();
-    let nameInput = document.querySelector('.popup__input_type_name');
-    let descriptionInput = document.querySelector('.popup__input_type_description');
-
-    profile.querySelector('.profile__name').textContent = nameInput.value;
-    profile.querySelector('.profile__description').textContent = descriptionInput.value;
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = descriptionInput.value;
     closePopup();
 }
 formElement.addEventListener('submit', addInfo);
-editButton.addEventListener('click', openPopup);
+popupOpened.addEventListener('click', openPopup);
 popupClose.addEventListener('click', closePopup);
