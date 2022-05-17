0eb7f09771493d1873dfef167298dffb8831034d
alsumukhamedova <alsumukhamedova@yandex.ru>
2022-05-18 00:15:29 +0300

    sprint 8

---
+++ src/components/Card.js
@@ -0,0 +1,38 @@
+export class Card {
+    constructor(name, link, cardSelector, handleImageClick) {
+        this._name = name;
+        this._link = link;
+        this._cardSelector = cardSelector;
+        this._handleImageClick = handleImageClick;
+    }
+    _getTemplate() {
+        return document.querySelector(this._cardSelector).content.querySelector('.element').cloneNode(true);
+    }
+    generateCard() {
+        this._element = this._getTemplate();
+        this._element.querySelector('.element__name').textContent = this._name;
+        this._element.querySelector('.element__image').src = this._link;
+        this._element.querySelector('.element__image').alt = this._name;
+        this._setEventListeners();
+        return this._element;
+    }
+    _deleteCard() {
+        this._element.remove();
+        this._element = null;
+    }
+    _likeCard() {
+        const buttonLike = this._element.querySelector('.element__like');
+        buttonLike.classList.toggle('element__like_active');
+    }
+    _setEventListeners() {
+        this._element.querySelector('.element__like').addEventListener('click', () => {
+            this._likeCard()
+        });
+        this._element.querySelector('.element__delete-card').addEventListener('click', () => {
+            this._deleteCard()
+        });
+        this._element.querySelector('.element__image').addEventListener('click', () => {
+            this._handleImageClick(this._link, this._name);
+        });
+    }
+}
---
+++ src/components/FormValidator.js
@@ -0,0 +1,60 @@
+export class FormValidator {
+    constructor(config, form) {
+        this._form = form;
+        this._config = config;
+        this._inputList = Array.from(this._form.querySelectorAll(this._config.inputElement));
+        this._buttonElement = this._form.querySelector(this._config.buttonElement);
+    }
+    _showInputError (inputElement){
+        inputElement.classList.add(this._config.inputErrorClass);
+        inputElement.classList.add(this._config.errorClass);}
+
+    _hideInputError(inputElement){
+        inputElement.classList.remove(this._config.inputErrorClass);
+        inputElement.classList.remove(this._config.errorClass);
+        inputElement.textContent = '';
+    };
+    _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+        this._showInputError(inputElement);
+    } else {
+        this._hideInputError(inputElement);
+    }};
+    _hasInvalidInput() {
+    return this._inputList.some((inputElement) => {
+        return !inputElement.validity.valid;
+    })};
+    _toggleButtonState() {
+            if (this._hasInvalidInput()) {
+                this._buttonElement.classList.add(this._config.inactiveButtonClass);
+                this._buttonElement.setAttribute("disabled", "disabled");
+            } else {
+                this._buttonElement.classList.remove(this._config.inactiveButtonClass);
+                this._buttonElement.removeAttribute("disabled");
+            }};
+    _setEventListeners() {
+        this._toggleButtonState();
+        this._inputList.forEach((inputElement) => {
+            inputElement.addEventListener('input', () => {
+                this._checkInputValidity(inputElement);
+                this._toggleButtonState();
+            });
+        });
+    };
+    enableValidation() {
+        this._form.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+        });
+        this._setEventListeners();
+    };
+    resetValidation() {
+        this._inputList.forEach((item) => {
+            item.classList.remove(this._config.inputErrorClass)
+        });
+        this._buttonElement.classList.add(this._config.inactiveButtonClass);
+        this._buttonElement.setAttribute("disabled", "disabled");
+        this._form.querySelectorAll('.popup__input_type_error').forEach((item) => {
+            item.classList.remove(this._config.errorClass)
+        });
+    }
+}
